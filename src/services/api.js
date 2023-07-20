30abf5a51d1aec798dc67b5b7cda060584593bd0
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2023-07-20 01:10:29 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,3 @@
-//017c3cc301e8494987e93bdd68c89dfd
-
-//https://api.openweathermap.org/data/2.5/forecast?q=Coromandel&units=metric&appid=7b8c0d8073cfe57c6d7f8765a07c55e6&lang=pt
 
 import axios from "axios";
 
